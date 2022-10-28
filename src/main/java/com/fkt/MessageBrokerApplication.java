81fa46514b031f26506b3a7dd68c8034151ef2e5
fan9704 <cxz123499@gmail.com>
2022-10-28 15:52:45 +0800

    Main Application SetUp Swagger Config

--- src/main/java/com/fkt/MessageBrokerApplication.java
+++ src/main/java/com/fkt/MessageBrokerApplication.java
@@ -2,12 +2,20 @@ package com.fkt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+@EnableSwagger2
+@EnableWebMvc
 @SpringBootApplication
 public class MessageBrokerApplication {
-
+    private static final Logger log = LoggerFactory.getLogger(MessageBrokerApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(MessageBrokerApplication.class, args);
+        log.info("Web Document Reference http://127.0.0.1:8081/swagger-ui/index.html");
+        log.info("Server is Running on http://127.0.0.1:8081");
     }
 
 }
